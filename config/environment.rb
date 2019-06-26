93583ab71953804bd9a981ba47fd2a55ae38b1d9
Jim Hough <teachlife@outlook.com>
2019-06-26 13:34:03 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 ENV['SINATRA_ENV'] ||= "development"
 
 configure :development do
-  set :database, 'sqlit3:db/database.db'
+  set :database, 'sqlite3:db/database.db'
 end
 
 require 'bundler/setup'
